1d5815deeba76b2b26011ee4bd2fbdeb466ee024
Jihwan Kim <noneobj@gmail.com>
2021-01-03 13:38:00 +0900

    Make directory for commands, write lines for commands

---
+++ commands/scar.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: '이상형',
+	description: '랄로의 이상형',
+	execute(msg, args) {
+        msg.channel.send('https://youtu.be/Rc8CSYe6INs?t=131')
+        msg.channel.send('말을 아끼겠습니다...')
+        msg.channel.send('그러니까 내가 누군가를 좋아한다는 사실이 그 사람에게는 상처가 될 수도 있잖아요...')
+	},
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const {token, prefix} = require("./config.json");
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// commands안의 모든 파일을 command로 등록
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  // 작성자가 봇이거나 prefix가 붙지 않은 명령어 무시
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.split(' ');
+  const command = args[1];
+  // 명령어에 붙은 args가 2개 이상이면 message 전달 후 무시
+  if (args.length !== 2) {
+    msg.reply('1개만 붙여라');
+    return;
+  }
+
+  try {
+    client.commands.get(command).execute(msg, '');
+  } catch (error) {
+    console.log(error)
+    msg.reply('네 해드렸습니다.');
+  }
+});
+
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`${member}.... 자~ 드가자~...`);
+  });
+
+client.login(token);
